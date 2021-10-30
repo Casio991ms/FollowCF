1879766d8540459e019203a51ba81e91e1f5d896
unknown <nafis.faiyaz.5@gmail.com>
2021-10-30 12:21:00 +0600

    worked on history buttons

--- followcf/lib/screens/history_page.dart
+++ followcf/lib/screens/history_page.dart
@@ -14,12 +14,13 @@ class UserHistory extends StatefulWidget {
 class _UserHistoryState extends State<UserHistory> {
   final DioClient _dioClient = DioClient();
   List<Submission> submissions = [];
+  int start = 1;
 
   @override
   void initState() {
     super.initState();
 
-    func(1).then((data) {
+    func(start).then((data) {
       setState(() {
         submissions = data;
       });
@@ -36,30 +37,71 @@ class _UserHistoryState extends State<UserHistory> {
       appBar: AppBar(
         title: Text(widget.handle),
       ),
-      body: ListView(
-        children: List.generate(
-            submissions.length,
-            (i) => Container(
-                  margin: EdgeInsets.fromLTRB(10, 3, 10, 3),
-                  decoration: const BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5)),
-                    color: Color(0xFFDDDDDD),
-                  ),
-                  child: ListTile(
-                    key: UniqueKey(),
-                    leading: Text(submissions[i].problemRating.toString()),
-                    title: Text(submissions[i].contestId.toString() +
-                        submissions[i].problemIndex),
-                    subtitle: Text(submissions[i].problemName),
-                    trailing: Text(
-                      submissions[i].verdict!,
-                      style: TextStyle(
-                          color: submissions[i].verdict! == "OK"
-                              ? Color(0xFF00AA00)
-                              : Color(0xFFFE0000)),
-                    ),
-                  ),
-                )),
+      body: Column(
+        children: [
+          Expanded(
+            child: Scrollbar(
+              isAlwaysShown: true,
+              child: ListView(
+                children: List.generate(
+                    submissions.length,
+                    (i) => Container(
+                          margin: const EdgeInsets.fromLTRB(10, 3, 10, 3),
+                          decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                            color: Color(0xFFDDDDDD),
+                          ),
+                          child: ListTile(
+                            key: UniqueKey(),
+                            leading:
+                                Text(submissions[i].problemRating.toString()),
+                            title: Text(submissions[i].contestId.toString() +
+                                submissions[i].problemIndex),
+                            subtitle: Text(submissions[i].problemName),
+                            trailing: Text(
+                              submissions[i].verdict!,
+                              style: TextStyle(
+                                  color: submissions[i].verdict! == "OK"
+                                      ? const Color(0xFF00AA00)
+                                      : const Color(0xFFFE0000)),
+                            ),
+                          ),
+                        )),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(10, 3, 10, 3),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                    onPressed: start == 1
+                        ? null
+                        : () {
+                            start -= 10;
+                            func(start).then((data) {
+                              setState(() {
+                                submissions = data;
+                              });
+                            });
+                          },
+                    icon: Icon(Icons.arrow_back)),
+                Text(((start + 9) ~/ 10).toString()),
+                IconButton(
+                    onPressed: () {
+                      start += 10;
+                      func(start).then((data) {
+                        setState(() {
+                          submissions = data;
+                        });
+                      });
+                    },
+                    icon: Icon(Icons.arrow_forward)),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
